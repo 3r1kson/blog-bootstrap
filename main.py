8ca59499b5452aab710785cd9253be70e48bb6ab
Erikson <eriksonribeiro84@gmail.com>
2025-03-04 02:53:05 -0300

    Updating message form sending email

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import requests
-from flask import Flask, render_template
+import smtplib
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+
+load_dotenv()
 
 app = Flask(__name__)
 
 posts_list = requests.get('https://api.npoint.io/c790b4d5cab58020d391').json()
+my_email = os.getenv("my_email")
+password = os.getenv("password")
+hotmail_email = os.getenv("hotmail_email")
 
 @app.route('/')
 def home():
@@ -13,9 +21,29 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET','POST'])
 def contact():
-    return render_template('contact.html')
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        message = request.form['message']
+
+        messageEmail = f"name: {name}\nemail: {email} \nphone: {phone}\nmessage: {message}"
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=email,
+                to_addrs=hotmail_email,
+                msg=f"Subject: Blog message \n\n{messageEmail}"
+            )
+
+        return render_template('contact.html', message="Successfully sent your message")
+    else:
+        return render_template('contact.html')
+
 
 @app.route('/post/<number>')
 def post(number):
@@ -29,5 +57,7 @@ def post(number):
                 'body': i['body']
             }
     return render_template('post.html', post=selected_post)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
